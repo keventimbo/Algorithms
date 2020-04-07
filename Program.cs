df3594a0ab0eb47eed48d291536098530f5c0aee
kevent <40048372+keventimbo@users.noreply.github.com>
2020-04-06 23:55:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+public class Idade
+{
+    public static void Main()
+    {
+        WriteLine("Qual Sua Idade ? :");
+        if (!int.TryParse(ReadLine(), out var n1)) return;
+        else if (n1 < 10) WriteLine("Você é Criança");
+        else if (n1 < 18) WriteLine("Você é Adolescente");
+        else WriteLine("Você é Adulto");
+    }
+}
